0bd054296b94793ab9b4fe47e31b27ae1bad86b9
snakajim <satoshi.nakajima@arm.com>
2022-01-16 04:47:48 +0000

    minor fix

--- docker_install.sh
+++ docker_install.sh
@@ -18,5 +18,5 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get -y update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 sudo chmod 777 /var/run/docker.sock
